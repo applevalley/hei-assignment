a734d59a1390312e7814dd34d77e20e0c3ea0d29
applevalley <skatkd7571@naver.com>
2024-12-23 12:44:27 +0900

    chore(assignment): fix lint error

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    "expo",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  ignorePatterns: ["babel.config.js", "/dist/*"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    "no-console": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+    "react/react-in-jsx-scope": "off",
+  },
+};
